3a0fd74b91624b25c473ea36f110f21093a0f84f
Merey <merey.90@gmail.com>
2018-09-01 15:25:13 +0200

    initial commit

---
+++ src/routes/answers.js
@@ -0,0 +1,10 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res, next) => {
+  res.send({ title: 'Answers' });
+});
+
+module.exports = router;
